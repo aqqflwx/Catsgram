442202addb1ddb699239942b2c54374fcfe17f1e
Andrew Vlasov <andrroiid@ya.ru>
2025-09-16 09:28:29 +0300

    feat: separation of classes into controllers and services

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,88 +1,34 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
-import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
-import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
 
-import java.time.Instant;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private final Map<Long, User> users = new HashMap<>();
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public Collection<User> findListUsers() {
-        return users.values();
+        return userService.findListUsers();
     }
 
     @PostMapping
-    public User addUser(@RequestBody User user) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ConditionsNotMetException("Имейл должен быть указан");
-        }
-
-        if (users.values().stream()
-                .anyMatch(userInList -> userInList.getEmail()
-                        .equals(user.getEmail()))) {
-            throw new DuplicatedDataException("Этот имейл уже используется");
-        }
-
-        user.setId(getNextId());
-        user.setRegistrationDate(Instant.now());
-
-        users.put(user.getId(), user);
-
-        return user;
+    public User addUser(User user) {
+        return userService.addUser(user);
     }
 
     @PutMapping
     public User update(@RequestBody User newUser) {
-        if (newUser.getId() == null) {
-            throw new ConditionsNotMetException("Id должен быть указан");
-        }
-
-        if (users.containsKey(newUser.getId())) {
-            User oldUser = users.get(newUser.getId());
-
-            if (users.values().stream()
-                    .anyMatch(user -> user.getEmail().equals(newUser.getEmail()))) {
-                throw new DuplicatedDataException("Этот имейл уже используется");
-            }
-
-            if (newUser.getEmail() != null) {
-                oldUser.setEmail(newUser.getEmail());
-            }
-
-            if (newUser.getUsername() != null) {
-                oldUser.setUsername(newUser.getUsername());
-            }
-
-            if (newUser.getPassword() != null) {
-                oldUser.setPassword(newUser.getPassword());
-            }
-
-            return oldUser;
-        }
-
-        throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
-    }
-
-
-    // вспомогательный метод для генерации идентификатора нового поста
-    private long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+        return userService.update(newUser);
     }
 
 }
